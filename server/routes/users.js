b9c0a76383bee5740efd1b270667073dc9d3afb9
aidos42 <75936428+aidos42@users.noreply.github.com>
2022-10-17 12:12:19 +0300

    add first iteration of user editing

--- server/routes/users.js
+++ server/routes/users.js
@@ -13,6 +13,24 @@ export default (app) => {
       const user = new app.objection.models.user();
       reply.render('users/new', { user });
     })
+    .get('/users/:id/edit', { name: 'editUser' }, async (req, reply) => {
+      const { id } = req.params;
+
+      try {
+        const user = await app.objection.models.user.query().findById(Number(id));
+
+        if (typeof user === 'undefined') {
+          throw Error('Wrong user id');
+        }
+
+        reply.render('users/edit', { user, id });
+      } catch (e) {
+        req.flash('error', i18next.t('flash.users.edit.errorOpen'));
+        reply.redirect(app.reverse('root'));
+      }
+
+      return reply;
+    })
     .post('/users', async (req, reply) => {
       const user = new app.objection.models.user();
       user.$set(req.body.data);
@@ -22,11 +40,23 @@ export default (app) => {
         await app.objection.models.user.query().insert(validUser);
         req.flash('info', i18next.t('flash.users.create.success'));
         reply.redirect(app.reverse('root'));
-      } catch ({ data }) {
+      } catch (e) {
+        const { data } = e;
         req.flash('error', i18next.t('flash.users.create.error'));
         reply.render('users/new', { user, errors: data });
       }
 
       return reply;
+    })
+    .patch('/users/:id', async (req, reply) => {
+      const { id } = req.params;
+      const { email: newEmail } = req.body;
+      // TODO: В дальнейшем дополнить поля
+      await app.objection.models.user.query().findById(Number(id))
+        .patch({
+          email: newEmail,
+        });
+      console.log('succesfull path');
+      reply.redirect(app.reverse('root'));
     });
 };
